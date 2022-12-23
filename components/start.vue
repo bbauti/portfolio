052861dc5d0da2858147b4e69a016a1ef2a27894
akabbauti <bautistaigarzabal11@gmail.com>
2022-12-23 00:26:15 -0300

    add tech stack

---
+++ components/start.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="header" id="header">
+        <h1 class="titulo">
+            Hola! Soy <span class="borde">Bautista Igarzabal</span>
+        </h1>
+        <h1 class="subtitulo">
+            Un
+            <a v-scroll-to="{
+                el: '#developer',
+                duration: 500,
+                easing: 'ease-in-out',
+                offset: -100
+            }" class="developer">desarrollador</a>
+            y
+            <a v-scroll-to="{
+                el: '#artist',
+                duration: 500,
+                easing: 'ease-in-out',
+                offset: -100
+            }" class="artist">artista</a>
+        </h1>
+        <div id="stackSection">
+            <h1 class="subtitulo" style="margin-bottom: 1vw">tech stack</h1>
+            <div class="techContainer">
+                <Icon icon="vscode-icons:file-type-vue" class="icons" />
+                <Icon icon="vscode-icons:file-type-nuxt" class="icons"  />
+                <Icon icon="vscode-icons:file-type-node" class="icons"  />
+                <Icon icon="vscode-icons:file-type-python" class="icons"  />
+                <Icon icon="vscode-icons:file-type-html" class="icons"  />
+                <Icon icon="vscode-icons:file-type-css" class="icons"  />
+                <Icon icon="logos:javascript" class="icons"  />
+                <Icon icon="vscode-icons:file-type-scss" class="icons"  />
+                <Icon icon="logos:bulma" class="icons"  />
+                <Icon icon="logos:postgresql" class="icons"  />
+                <Icon icon="logos:docker-icon" class="icons"  />
+                <Icon icon="logos:bash-icon" class="icons"  />
+                <Icon icon="logos:apache" class="icons"  />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { Icon } from '@iconify/vue2';
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,7 @@
 <template>
 	<div class="container">
 		<div class="main">
-            <div class="header" id="header">
-                <h1 class="titulo">
-                    Hola! Soy <span class="borde">Bautista Igarzabal</span>
-                </h1>
-                <h1 class="subtitulo">
-                    Un
-                    <a v-scroll-to="{
-                        el: '#developer',
-                        duration: 500,
-                        easing: 'ease-in-out',
-                        offset: -100
-                    }" class="developer">desarrollador</a>
-                    y
-                    <a v-scroll-to="{
-                        el: '#artist',
-                        duration: 500,
-                        easing: 'ease-in-out',
-                        offset: -100
-                    }" class="artist">artista</a>
-                </h1>
-            </div>
+            <start />
             <projects />
             <photos />
             <contact />
