94002e5c38af619e473f620221da2f374897e544
bbauti <bautistaigarzabal11@gmail.com>
2024-01-23 20:20:28 -0300

    fix vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import vue from "@astrojs/vue";
-
 import tunnel from "astro-tunnel";
+
+import vercelStatic from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,6 +13,13 @@ export default defineConfig({
   }), icon(), vue(), tunnel({
     url: 'http://localhost:4321',
     port: 4321,
-    acceptCloudflareNotice: true,
-  })]
+    acceptCloudflareNotice: true
+  })],
+  output: 'static',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    maxDuration: 8,
+  }),
 });
